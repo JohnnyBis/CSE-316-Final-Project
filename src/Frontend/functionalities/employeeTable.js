c646251e7ddf307c08ef5682738239fd926d5ca5
Gianmaria Biselli <gianmariabiselli@gmail.com>
2020-11-28 19:14:43 -0500

    Forgot to add file - same commit as before.

--- src/Frontend/functionalities/employeeTable.js
+++ src/Frontend/functionalities/employeeTable.js
@@ -7,28 +7,47 @@ function fetchSavedTests() {
         return result.json();
     }).then(function (data) {
         return data;
+    }).catch((error) => {
+        console.log(error);
+        return null;
     });
 }
 
 async function displaySavedTests() {
     const $clone = $tableID.find('tbody tr').last().clone(true).removeClass('hide table-line');
     let fetchTestList = await fetchSavedTests();
-    console.log(fetchTestList);
+    if (fetchTestList == null) return;
+
     if ($tableID.find('tbody tr').length === 0) {
         for (let i = 0; i < fetchTestList.length; i++) {
             const newTr = `
-                <tr class="hide">
-                <td class="pt-3-half">${fetchTestList[i]["employeeID"]}</td>
-                <td class="pt-3-half">${fetchTestList[i]["testBarcode"]}</td>
-                <td>
-                    <span class="table-remove"><button type="button" class="btn btn-danger btn-rounded btn-sm my-0 waves-effect waves-light">Remove</button></span>
-                </td>
+                <tr class="hide" id="${fetchTestList[i]["testBarcode"]}">
+                    <td class="pt-3-half">${fetchTestList[i]["employeeID"]}</td>
+                    <td class="pt-3-half">${fetchTestList[i]["testBarcode"]}</td>
+                    <td>
+                        <span class="table-remove"><button type="button" class="btn btn-danger btn-rounded btn-sm my-0 waves-effect waves-light">Remove</button></span>
+                    </td>
                 </tr>`;
             $('tbody').append(newTr);
         }
     }
-
     $tableID.find('table').append($clone);
+}
+
+function deleteTestBy(barcodeID) {
+    return fetch(`http://localhost:8080/deleteTest/${barcodeID}`).then(result => {
+        return result;
+    }).catch((error) => {
+        console.log(error);
+        return null;
+    });
+}
+
+async function deleteSavedTest(element, barcodeID) {
+    let result = await deleteTestBy(barcodeID);
+    console.log(result);
+    if(result == null) return;
+    $(element).parents('tr').detach();
 }
 
 displaySavedTests();
@@ -37,13 +56,14 @@ $('#form-submit').on('click', async () => {
     displaySavedTests();
 });
 
-$tableID.on('click', '.table-remove', function () {
-    $(this).parents('tr').detach();
+$tableID.on('click', '.table-remove', async function () {
+    let testID = $(this).parents('tr').attr('id');
+    deleteSavedTest(this, testID);
 });
 
 $tableID.on('click', '.table-up', function () {
     const $row = $(this).parents('tr');
-
+    
     if ($row.index() === 0) {
         return;
     }
